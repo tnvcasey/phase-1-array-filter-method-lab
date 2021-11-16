8c1c1c2fd534f4453d2d43156e1c3a9e42bbe41a
taylorvercillo <taylornvcasey@gmail.com>
2021-11-15 20:37:10 -0700

    Almost done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+
+const drivers = ['Bobby', 'Sally', 'Carol', 'Bobby'];
+
+function findMatching(drivers, name) {
+    const newDrivers = drivers.filter(person => person.toLowerCase() === name.toLowerCase()); 
+    return newDrivers; 
+}
+
+const fuzzyMatch = (drivers, name) => {
+    return drivers.filter(el => el.toLowerCase().indexOf(name.toLowerCase()) != -1)
+}
